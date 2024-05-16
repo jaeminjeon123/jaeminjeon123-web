ef42c069aa9cc28449fc3b5b57adf63d65e33ec9
jjm <jaeminjeon123@gmail.com>
2024-05-16 20:46:43 +0900

    feat: 회고롤 제외한 프로젝트 목록

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -5,19 +5,26 @@ import './Project.css';
 const ProjectList = () => {
   const projects = [
     {
-      title: "TODO LIST",
+      title: "병해 탐지 및 피해 현황 파악 서비스",
       description: "리액트를 학습하며 제작한 데스크탑 페이지입니다.",
       features: ["할 일 추가", "삭제", "체크", "중요표시"],
-      link: "https://github.com/your-github-repo",
-      image: `${process.env.PUBLIC_URL}/images/todo-list.jpg`
+      link: "https://www.notion.so/Projects-9f656dfc7e5647099683985880edfff5",
+      image: `${process.env.PUBLIC_URL}/first.png`
     },
     {
-      title: "Another Project",
+      title: "커리어허브",
       description: "Another project description goes here.",
       features: ["Feature 1", "Feature 2", "Feature 3"],
-      link: "https://github.com/another-github-repo",
-      image: `${process.env.PUBLIC_URL}/images/another-project.jpg`
+      link: "https://github.com/jaeminjeon123/CCC",
+      image: `${process.env.PUBLIC_URL}/first.png`
     },
+    {
+        title: "tetrist",
+        description: "Another project description goes here.",
+        features: ["Feature 1", "Feature 2", "Feature 3"],
+        link: "https://github.com/jaeminjeon123/CCC",
+        image: `${process.env.PUBLIC_URL}/tetrist.png`
+      },
     // 더 많은 프로젝트를 추가할 수 있습니다.
   ];
 
